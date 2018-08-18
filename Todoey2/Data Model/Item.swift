1e08450ffe47f48e814a2becf2499e0a92cc4c76
James Buckley <j@mesbuckley.com>
2018-08-18 01:51:17 +0100

    Convert to MVC design pattern.

---
+++ Todoey2/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey2
+//
+//  Created by James Buckley on 18/08/2018.
+//  Copyright © 2018 James Buckley. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
